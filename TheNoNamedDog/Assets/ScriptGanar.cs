923def8a45082249d6ef767d88b6a9499e2bfc4b
Mrcuerno <38971087+Mrcuerno@users.noreply.github.com>
2020-04-03 10:48:21 -0600

    FinalMultiplayerP1
    Mapa,ganar,anim

---
+++ TheNoNamedDog/Assets/ScriptGanar.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ScriptGanar : MonoBehaviour
+{
+
+    public GameObject panel,ganar1,ganar2;
+    public GameObject Player1, Player2;
+    public float times;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        panel.SetActive(false);
+        ganar1.SetActive(false);
+        ganar2.SetActive(false);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Entre");
+            panel.SetActive(true);
+            StartCoroutine(DoSomething(times));
+            
+            if (other.gameObject == Player1)
+            {
+                ganar1.SetActive(true);
+                //if (Input.anyKey)
+                //{
+                //    SceneManager.LoadScene("Tutorial 1", LoadSceneMode.Additive);
+                //}
+            }
+            if(other.gameObject == Player2)
+            {
+                ganar2.SetActive(true);
+            }
+
+            
+        }
+
+    }
+
+    private IEnumerator DoSomething(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("CharacterChoose");
+    }
+}
